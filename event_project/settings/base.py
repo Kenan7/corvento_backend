7a26c8f69392c813d5b0e19b486d4f01a698a877
Kenan7 <kenankzmv@gmail.com>
2020-04-05 02:55:58 +0400

    [refactor]

--- event_project/settings/base.py
+++ event_project/settings/base.py
@@ -1,17 +1,19 @@
 from .parse_secret_json import *
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
 
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
-STATIC_ROOT = BASE_DIR / 'static_root'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+LOCAL = [
+    'main.apps.MainConfig',
+]
 
-
-DEBUG = True
-
-ALLOWED_HOSTS = []
+THIRD_PARTY = [
+    'rest_framework',
+    'versatileimagefield',
+    'django_filters',
+]
 
 BASE = [
     'django.contrib.admin',
@@ -22,16 +24,21 @@ BASE = [
     'django.contrib.staticfiles',
 ]
 
-LOCAL = [
-    'main.apps.MainConfig',
-]
-
-THIRD_PARTY = [
-    'rest_framework',
-    'versatileimagefield',
-]
-
 INSTALLED_APPS = BASE + LOCAL + THIRD_PARTY
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_DIR / 'static_root'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+
+
+DEBUG = True
+ALLOWED_HOSTS = []
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -64,20 +71,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'event_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -94,17 +87,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Asia/Baku'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-STATIC_URL = '/static/'
