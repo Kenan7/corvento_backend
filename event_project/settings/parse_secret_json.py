198ccb07bb7524bb0e51c20088653bbfe95f5af4
Kenan7 <kenankzmv@gmail.com>
2020-04-04 23:31:31 +0400

    [refactor]

--- event_project/settings/parse_secret_json.py
+++ event_project/settings/parse_secret_json.py
@@ -35,17 +35,17 @@ DATABASES = {
 # }
 
 
-AWS_ACCESS_KEY_ID = get_secret('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = get_secret('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = get_secret('AWS_STORAGE_BUCKET_NAME')
+# AWS_ACCESS_KEY_ID = get_secret('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = get_secret('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = get_secret('AWS_STORAGE_BUCKET_NAME')
 
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % get_secret(
-    'AWS_S3_CUSTOM_DOMAIN'
-)
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % get_secret(
+#     'AWS_S3_CUSTOM_DOMAIN'
+# )
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400',
+# }
+# AWS_LOCATION = 'static'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
