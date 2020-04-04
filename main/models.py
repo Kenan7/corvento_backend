1a205f627f89038ba1accc9775566b7c81f9c75e
Kenan7 <kenankzmv@gmail.com>
2020-04-04 21:13:53 +0400

    [feat] Add Event Model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from versatileimagefield.fields import VersatileImageField
+from django.conf import settings
 
-# Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+
+class Event(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    desc = models.CharField(max_length=1024)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    image = VersatileImageField(
+        upload_to="event_images", blank=True
+    )
+    venue = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
