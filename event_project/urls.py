0e0a975ceba5e84d5b5e79ea12a4848a718d44c8
Kenan7 <kenankzmv@gmail.com>
2020-05-02 18:13:24 +0400

    [feat] Add index and contact form urls

--- event_project/urls.py
+++ event_project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from app_user import views
-
+from main import views as main_views
 from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Event Project API')
@@ -10,11 +10,13 @@ schema_view = get_swagger_view(title='Event Project API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_views.Home.as_view()),
     path('api/docs/', schema_view),
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/events/', include('main.urls')),
     path('api/users/', include('app_user.urls')),
     path('api/categories/', include('main.url_category')),
+    path('api/form/', include('app_user.url_form')),
 ]
 
 if settings.DEBUG:
