d1dd28423396222137e06df0705f5979e4a8869d
Kenan7 <kenankzmv@gmail.com>
2020-05-09 15:02:27 +0400

    add a few urls

--- event_project/urls.py
+++ event_project/urls.py
@@ -1,3 +1,4 @@
+from rest_framework.routers import DefaultRouter
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -10,13 +11,16 @@ schema_view = get_swagger_view(title='Event Project API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main_views.Home.as_view()),
+    path('', main_views.Home.as_view(), name='home'),
+    path('tinymce/', include('tinymce.urls')),
     path('api/docs/', schema_view),
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/events/', include('main.urls')),
     path('api/users/', include('app_user.urls')),
     path('api/categories/', include('main.url_category')),
     path('api/form/', include('app_user.url_form')),
+    path('privacy-and-policy', main_views.Policy.as_view(), name='policy'),
+    path('terms-and-conditions', main_views.Terms.as_view(), name='terms'),
 ]
 
 if settings.DEBUG:
